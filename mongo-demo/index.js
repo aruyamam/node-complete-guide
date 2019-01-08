cf1617cd0c7b8a8e2aec16252c2c306a4e36b403
aruyamam <aruyamam@gmail.com>
2019-01-08 12:24:56 +0900

    Validation

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -9,14 +9,52 @@ mongoose
    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
-   name: String,
+   name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      // match: /patter/
+   },
+   category: {
+      type: String,
+      required: true,
+      enum: ['web', 'mobile', 'network'],
+      lowercase: true,
+      // uppercase: true,
+      trim: true,
+   },
    author: String,
-   tags: [String],
+   tags: {
+      type: Array,
+      validate: {
+         isAsync: true,
+         validator(v, callback) {
+            setTimeout(() => {
+               // Do some async work
+               const result = v && v.length > 0;
+               callback(result);
+            }, 4000);
+            // return v && v.length > 0;
+         },
+         message: 'A course should have at least one tag.',
+      },
+   },
    date: {
       type: Date,
       default: Date.now,
    },
    isPublished: Boolean,
+   price: {
+      type: Number,
+      required() {
+         return this.isPublished;
+      },
+      min: 10,
+      max: 200,
+      get: v => Math.round(v),
+      set: v => Math.round(v),
+   },
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -24,13 +62,22 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
    const course = new Course({
       name: 'Angular Course',
+      category: 'Web',
       author: 'Mosh',
-      tags: ['angular', 'frontend'],
+      tags: ['frontend'],
       isPublished: true,
+      price: 15.8,
    });
 
-   const result = await course.save();
-   console.log(result);
+   try {
+      const result = await course.save();
+      console.log(result);
+   }
+   catch (ex) {
+      for (field in ex.errors) {
+         console.log(ex.errors[field].message);
+      }
+   }
 }
 
 // createCourse();
@@ -65,12 +112,15 @@ async function getCourses() {
    // .sort({ name: 1 }) // -1
    // .select({ name: 1, tags: 1 });
    // .count();
-   const courses = await Course.find();
+   const courses = await Course.find()
+      .find({ _id: '5c34163a55cb553a281f2460' })
+      .sort({ name: 1 })
+      .select({ name: 1, tags: 1, price: 1 });
 
-   console.log(courses);
+   console.log(courses[0].price);
 }
 
-// getCourses();
+getCourses();
 
 async function updateCourse(id) {
    // query first
@@ -117,4 +167,4 @@ async function removeCourse(id) {
    console.log(course);
 }
 
-removeCourse('5c2eba68f8adf450947e7fa4');
+// removeCourse('5c2eba68f8adf450947e7fa4');
